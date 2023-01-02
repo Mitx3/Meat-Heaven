eeafef0a8cb6e78d33e700d054a201a907d6c3a8
Mitjan <mitjan.stepancic@gmail.com>
2023-01-02 19:49:28 +0100

    Comment Test

--- web/Program.cs
+++ web/Program.cs
@@ -5,7 +5,7 @@ using web.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//var connectionString = builder.Configuration.GetConnectionString("TrgovinaContext");
+//var connectionString = builder.Configuration.GetConnectionString("TrgovinaContext"); Local
 var connectionString = builder.Configuration.GetConnectionString("AzureContext");
 
 
